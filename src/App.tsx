ab5cfa555af900a9acdccd2f25d1f365b988f1ea
Michał Mirosław <michal@majk.eu>
2025-06-04 19:48:12 +0200

    create custom hook to fetch games

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import {Grid, GridItem, Show} from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
+import GameGrid from "./components/GameGrid";
 
 function App() {
     return (
@@ -17,7 +18,7 @@ function App() {
                 </GridItem>
             </Show>
             <GridItem area='main'>
-                Main
+                <GameGrid></GameGrid>
             </GridItem>
         </Grid>
     )
---
+++ src/services/api-client.ts
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL: 'https://api.rawg.io/api',
+    params: {
+        key: '35e75bfe4089411e99aa44d391b2cc68'
+    },
+})
