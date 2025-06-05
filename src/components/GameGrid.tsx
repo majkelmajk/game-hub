2f302f0410f6c1dc01d85a5971797d8c0e8fd336
Michał Mirosław <michal@majk.eu>
2025-06-06 00:01:36 +0200

    cleanup game cards

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -16,7 +16,7 @@ function GameGrid({gameQuery}: Props) {
     return (
         <>
             {error && <Text>{error}</Text>}
-            <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} spacing={3} padding={2}>
+            <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={6} padding={2}>
                 {loading && skeletons.map(s =>
                     <GameCardContainer key={s} >
                         <GameCardSkeleton/>
