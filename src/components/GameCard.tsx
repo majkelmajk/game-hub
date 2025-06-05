3ab87c0b0103b4b49fae5268b0205ef4e86f428b
Michał Mirosław <michal@majk.eu>
2025-06-05 23:04:04 +0200

    handle games with no images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -4,15 +4,19 @@ import {Card, CardBody, Heading, HStack, Image} from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
+import placeholderImage from "../assets/game_placeholder.png"
 
 interface GameCardProps {
     game: Game
 }
 
 function GameCard({game}: GameCardProps) {
+    const image = game.background_image
+        ? getCroppedImageUrl(game.background_image)
+        : placeholderImage;
     return (
         <Card>
-            <Image src={getCroppedImageUrl(game.background_image)}/>
+            <Image src={image}/>
             <CardBody>
                 <Heading fontSize='2xl'>{game.name}</Heading>
                 <HStack justifyContent={'space-between'}>
