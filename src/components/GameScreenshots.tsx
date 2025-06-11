fadc6828bd93c79c738b93d42a1bf00878063548
Michał Mirosław <michal@majk.eu>
2025-06-11 21:30:32 +0200

    game screenshots

---
+++ src/components/GameScreenshots.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import useScreenshots from "../hooks/useScreenshots";
+import {Image, SimpleGrid} from "@chakra-ui/react";
+import {Screenshot} from "../entities/Screenshot";
+
+interface Props {
+    gameId: number;
+}
+
+const GameScreenshots = ({gameId}: Props) => {
+    const {data, error, isLoading} = useScreenshots(gameId)
+
+    if (isLoading) return null
+    if (error) throw error
+
+    return (
+        <SimpleGrid columns={{base: 1, md: 2}} spacing={2}>
+            { data?.results?.map((screenshot: Screenshot) => (
+                <Image src={screenshot.image} key={screenshot.id} />
+            ))}
+        </SimpleGrid>
+    )
+};
+
+export default GameScreenshots;
---
+++ src/hooks/useScreenshots.ts
@@ -0,0 +1,16 @@
+import APIClient from "../services/api-client";
+import {Screenshot} from "../entities/Screenshot";
+import {useQuery} from "@tanstack/react-query";
+import ms from "ms";
+
+const useScreenshots = (gameId: number) => {
+    const apiClient = new APIClient<Screenshot>(`/games/${gameId}/screenshots`)
+
+    return useQuery({
+        queryKey: ['screenshots', gameId],
+        queryFn: apiClient.getAll,
+        staleTime: ms('24h')
+    })
+}
+
+export default useScreenshots
