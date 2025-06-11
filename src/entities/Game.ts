5f841710485a3e02fd5dd5ac7519c4c460d316f5
Michał Mirosław <michal@majk.eu>
2025-06-11 20:56:45 +0200

    game attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,12 +1,16 @@
 import {Platform} from "../hooks/useGames";
+import {Genre} from "./Genre";
+import {Publisher} from "./Publisher";
 
 export interface Game {
     id: number;
     name: string;
     slug: string;
+    genres: Genre[]
     description_raw: string;
     background_image: string;
     parent_platforms: { platform: Platform }[];
+    publishers: Publisher[];
     metacritic: number;
     rating_top: number;
     rating: number;
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -2,20 +2,23 @@ import {useParams} from "react-router-dom";
 import useGame from "../hooks/useGame";
 import {Heading, Spinner} from "@chakra-ui/react";
 import ExpandableText from "../components/ExpandableText";
+import React from "react";
+import GameAttributes from "../components/GameAttributes";
 
 const GameDetailPage = () => {
     const {slug} = useParams();
     const {data, error, isLoading} = useGame(slug!)
 
-    if (isLoading) return <Spinner />
+    if (isLoading) return <Spinner/>
     if (error) throw error
 
     const game = data!
 
     return (
         <>
-            <Heading>{ game.name }</Heading>
-            <ExpandableText>{ game.description_raw }</ExpandableText>
+            <Heading>{game.name}</Heading>
+            <ExpandableText>{game.description_raw}</ExpandableText>
+            <GameAttributes game={game} />
         </>
     )
 }
